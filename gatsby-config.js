31d2dda837a6e38f3e714b80011fdf97cb1d3aad
hari8697 <hari8697@gmail.com>
2022-02-21 12:11:58 +0530

    fix: Styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,45 @@
 module.exports = {
-    siteMetadata: {
-        title: `Lamborghini`,
-        description: `The Aventador SVJ`,
-        author: `Hari Krishnan (deathspacedesign@gmail.com)`,
+  siteMetadata: {
+    title: `Lamborghini`,
+    description: `The Aventador SVJ`,
+    author: `Hari Krishnan (deathspacedesign@gmail.com)`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
-    plugins: [
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/lamborghini/logo.svg`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        custom: {
+          families: ["Circular"],
+          urls: ["/fonts/fonts.css"],
         },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/lamborghini/logo.svg`, // This path is relative to the root of the site.
-            },
-        },
-        {
-            resolve: "gatsby-plugin-web-font-loader",
-            options: {
-                custom: {
-                    families: ["Circular"],
-                    urls: ["/fonts/fonts.css"],
-                },
-            },
-        },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ],
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
